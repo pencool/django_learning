a7cef0f05afdb214320dce40aa03208aa0e28b8a
pencool <pencool2@yandex.ru>
2022-06-14 18:47:33 +0300

    first_commit

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 
@@ -61,3 +61,21 @@ def new_entry(request, topic_id):
 
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Редактирует существующую запись."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if request.method != 'POST':
+        # Исходный запрос; форма заполняется данными текущей записи.
+        form = EntryForm(instance=entry)
+    else:
+        # Отправка данных POST; обработать данные.
+        form = EntryForm(instance=entry,data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_log:topic', args=[topic.id]))
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
